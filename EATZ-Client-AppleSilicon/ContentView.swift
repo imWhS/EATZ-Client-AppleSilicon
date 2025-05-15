ff954194a185a653f6c071e317486ef6679edbcc
imWhS <heextory@icloud.com>
2025-05-15 19:56:06 +0900

    Initial Commit

---
+++ EATZ-Client-AppleSilicon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EATZ-Client-AppleSilicon
+//
+//  Created by 손원희 on 5/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EATZ-Client-AppleSilicon/EATZ_Client_AppleSiliconApp.swift
@@ -0,0 +1,17 @@
+//
+//  EATZ_Client_AppleSiliconApp.swift
+//  EATZ-Client-AppleSilicon
+//
+//  Created by 손원희 on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct EATZ_Client_AppleSiliconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

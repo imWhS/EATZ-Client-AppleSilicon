698b2c6a4390eafbe1e48a23ed3d4e6584a64176
imWhS <heextory@icloud.com>
2026-07-22 22:57:43 +0900

    refactor: RecipeEditorImageView 로컬 이미지(UIImage) 지원 추가

--- EATZ-Client-AppleSilicon/Views/RecipeEditor/RecipeEditorImageView.swift
+++ EATZ-Client-AppleSilicon/Views/RecipeEditor/RecipeEditorImageView.swift
@@ -9,24 +9,36 @@ import SwiftUI
 import Kingfisher
 
 struct RecipeEditorImageView: View {
+    let uiImage: UIImage?
     let imageUrl: String?
     
+    init (_ uiImage: UIImage) {
+        self.uiImage = uiImage
+        self.imageUrl = nil
+    }
+    
     init(_ imageUrl: String) {
+        self.uiImage = nil
         self.imageUrl = imageUrl
     }
     
     var body: some View {
         Group {
-            KFImage(URL(imageUrlString: imageUrl))
-                .placeholder {
-                    ProgressView("대표 사진을 불러오고 있어요...")
-                        .foregroundStyle(Color.init(hex: "BEBEB9"))
-                }
-                .resizable()
-                .aspectRatio(1, contentMode: .fit)
-                .clipped()
-                .ignoresSafeArea()
+            if let uiImage = uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+            } else {
+                KFImage(URL(imageUrlString: imageUrl))
+                    .placeholder {
+                        ProgressView("대표 사진을 불러오고 있어요...")
+                            .foregroundStyle(Color.init(hex: "BEBEB9"))
+                    }
+                    .resizable()
+            }
         }
+        .aspectRatio(1, contentMode: .fit)
+        .clipped()
+        .ignoresSafeArea()
         .background(Color.init(hex: "F9F9F9"))
         .clipShape(RoundedRectangle(cornerRadius: 10))
     }
